d496dc7abc4c84e9fa774133f73350697fe0f6a4
Guillaume <guillaumedelvaux@hotmail.com>
2021-02-07 22:52:36 +0200

    day1 layout learning

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="child1">
+        <span className="innertext">1</span>
+
+      </div>
+      <div className="child2">
+        <span className="innertext">2</span>
+
+      </div>
+      <div className="child3">
+        <span className="innertext">3</span>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
