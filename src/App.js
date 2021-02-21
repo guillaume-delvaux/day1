875e8a0a1c409b31a2f2944fd36fe2b5710af728
Guillaume <guillaumedelvaux@hotmail.com>
2021-02-21 20:45:28 +0200

    homework

--- src/App.js
+++ src/App.js
@@ -5,14 +5,70 @@ function App() {
     <div className="App">
       <div className="child1">
         <span className="innertext">1</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
 
       </div>
       <div className="child2">
         <span className="innertext">2</span>
+        <div className="green">
+          <div className="baby" ></div>
+          <div className="baby" ></div>
+          <div className="baby" ></div>
+        </div>
 
       </div>
       <div className="child3">
         <span className="innertext">3</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+
+      </div>
+      <div className="child1">
+        <span className="innertext">4</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+
+      </div>
+      <div className="child2">
+        <span className="innertext">5</span>
+        <div className="green">
+          <div className="baby" ></div>
+          <div className="baby" ></div>
+          <div className="baby" ></div>
+        </div>
+      </div>
+      <div className="child3">
+        <span className="innertext">6</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+
+      </div>
+      <div className="child1">
+        <span className="innertext">7</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+
+      </div>
+      <div className="child2">
+        <span className="innertext">8</span>
+        <div className="green">
+<div className="baby" ></div>
+      <div className="baby" ></div>
+      <div className="baby" ></div>
+   </div>
+
+      </div>
+      <div className="child3">
+        <span className="innertext">9</span>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
+        <div className="baby" ></div>
 
       </div>
 
